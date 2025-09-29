c590d6c37770241a2c7e3c870ca563dbee1e2a93
Olzhas-Kdyr <kdiroljas@gmail.com>
2025-09-29 14:31:56 +0500

    munch: Switch to munch_defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,7 @@ TARGET_SCREEN_DENSITY := 440
 
 
 # Kernel
-TARGET_KERNEL_CONFIG += vendor/xiaomi/munch.config
+TARGET_KERNEL_CONFIG += vendor/munch_defconfig
 
 # Miui Camera
 TARGET_INCLUDES_MIUI_CAMERA := true
